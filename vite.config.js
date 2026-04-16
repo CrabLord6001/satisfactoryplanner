e343bd32f3d0f3cfae641fd329352ac2c410673e
Jonathan Crow <Jonathanecrow@yahoo.com>
2026-04-15 23:15:20 -0400

    fix: use relative base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // IMPORTANT: Change 'satisfactory-planner' below to match your GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/satisfactory-planner/',
+  base: './',
 })
